b6803c3cc549e1bee477356365778c3a6af9c19b
Jordan Wilhite <jordanwilhite@gmail.com>
2015-07-31 16:39:51 -0500

    added views and models

--- src/js/views/tweet-view.js
+++ src/js/views/tweet-view.js
@@ -1,1 +1,9 @@
 'use strict'
+
+import import Router from '../routers/router.js'
+import Usermodel fom '../models/user-model'
+
+var TweetsView = Backbone.View.entend({
+  template = _.template(['#twitterfeed'].html()),
+  
+})
