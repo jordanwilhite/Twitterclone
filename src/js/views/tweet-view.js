6c6cba07acc36f9e7a9b71e3ef5986f788d3ad5b
Jordan Wilhite <jordanwilhite@gmail.com>
2015-08-03 11:24:52 -0500

    tweet-view addition

--- src/js/views/tweet-view.js
+++ src/js/views/tweet-view.js
@@ -1,9 +1,28 @@
 'use strict'
 
 import Router from '../routers/router.js'
-import Usermodel from '../models/user-model'
+import Usermodel from '../models/user-model';
+import Users frim '../collections/users';
 
-var TweetsView = Backbone.View.entend({
+let TweetsView = Backbone.View.entend({
   template = _.template($('#twitterfeed').html()),
 
+  tagName 'ul',
+
+  initialize: function(){
+    this.listenTo(this.collection 'TweetCollection', this.respose);
+  },
+
+  addAll: function:() {
+    this.collection.each(function(tweet) {
+      this.FeedView(tweet);
+    }, this);
+  }
+  
+  render: function() {
+    return this;
+  }
+}
 });
+
+export default UserView;
