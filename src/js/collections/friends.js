b5bdafb8f42659ad63d59f5bf12c89ffc757a167
jeremybdavis <jdavis129@gmail.com>
2015-08-04 09:13:33 -0500

    x

--- src/js/collections/friends.js
+++ src/js/collections/friends.js
@@ -1,7 +1,5 @@
-'use strict';
-
 import FriendModel from '../models/friend-model';
-import UserView from '../views/friend-view';
+import FriendView from '../views/friend-view';
 
 let Friends = Backbone.Collection.extend ({
   url: 'https://twitterfeeder.herokuapp.com/users',
--- src/js/models/friend-model.js
+++ src/js/models/friend-model.js
@@ -1,12 +1,15 @@
+import FriendView from '../views/friend-view';
+import Friends from '../collections/friends';
+
 let FriendModel = Backbone.Model.extend({
 
   url: 'https://twitterfeeder.herokuapp.com/users',
 
   listFriends: function(credentials) {
     $.ajax({
-      method: "GET",
-      url: "https://twitterfeeder.herokuapp.com/users",
-      dataType: "json",
+      method: 'GET',
+      url: 'https://twitterfeeder.herokuapp.com/users',
+      dataType: 'json',
       data: {
         user: {
           username: username.credentials,
