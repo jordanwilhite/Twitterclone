2f8e7c55d9da0279d80a72edd5f22216bdf51ec2
Jordan Wilhite <jordanwilhite@gmail.com>
2015-08-07 10:28:57 -0500

    added feed view script & changed to ES6 on signip and signin

--- src/js/views/feed-view.js
+++ src/js/views/feed-view.js
@@ -1,10 +1,31 @@
+'use strict'
+
 import UserModel from '../models/user-model';
 import Router from '../routers/router';
 import TweetView from '../views/tweet-view';
 import TweetsCollection from '../collections/tweets';
 
-var FeedView = Backbone.View.extend({
+let FeedView = Backbone.View.extend({
 template: _.template($('#feedview').html()),
+
+tagName: 'ul',
+
+initialize: function() {
+  this.listenTo(this.collection, 'TweetsCollection', this.addTweets);
+
+},
+
+addAll: function(){
+  this.collection.each(function(user){
+    this.addTweets(tweet);
+  }, this)
+
+}
+
+addTweets: function() {
+  var tweets = new Tweet ({model:tweet-model});
+  this.$el.append(view.render().el);
+},
 
 });
 
